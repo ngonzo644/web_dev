cd8844f3f187d4e5e7767cea52d1b8d0aff81bda
Nicholas Gonzalez <nichoja20@gmail.com>
2025-03-10 22:05:59 -0400

    simple todoList

--- js_tutorial/todo.js
+++ js_tutorial/todo.js
@@ -1,8 +1,21 @@
 let todoList = [];
-function add_todo()
+function add_todo_1()
 {
-  let input = document.querySelector('.search');
+  let input = document.querySelector('.search-1');
   input = input.value; 
   todoList.push(input);
   console.log(todoList);
+}
+function add_todo_2()
+{
+  let input = document.querySelector('.search-2');
+  input=input.value;
+  todoList.push(input);
+  let todoHTML = '';
+  for(let i=todoList.length-1; i>=0; i--)
+  {
+    let html = `<p>${todoList[i]}</p>`;
+    todoHTML += html;
+    document.querySelector('.todo').innerHTML=todoHTML;
+  }
 }
