97d02a66cdef1262987e65714399b93f4b39ecf4
Nicholas Gonzalez <nichoja20@gmail.com>
2025-06-17 20:01:04 -0400

    use products.js to load in data

--- js_tutorial/javascript-amazon-project-main/scripts/amazon.js
+++ js_tutorial/javascript-amazon-project-main/scripts/amazon.js
@@ -1,33 +1,43 @@
 // an array of products (that are objects)
-const products = [
-  {
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars : 4.5,
-    count: 87
-  },
-  price: 1090 // because 10.90 cant be done in JS
+// const products = [
+//   {
+//   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+//   name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+//   rating: {
+//     stars : 4.5,
+//     count: 87
+//   },
+//   price: 1090 // because 10.90 cant be done in JS
 
-},
-{
-  image : "images/products/intermediate-composite-basketball.jpg",
-  name : "Intermediate Size Basketball",
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  price: 2095
-},
-{
-  image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-  name : "Adults Plain Cotton T-Shirt - 2 Pack",
-  rating : {
-    stars: 4.5,
-    count: 56
-  },
-  price: 799
-}];
+// },
+// {
+//   image : "images/products/intermediate-composite-basketball.jpg",
+//   name : "Intermediate Size Basketball",
+//   rating: {
+//     stars: 4,
+//     count: 127
+//   },
+//   price: 2095
+// },
+// {
+//   image : "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//   name : "Adults Plain Cotton T-Shirt - 2 Pack",
+//   rating : {
+//     stars: 4.5,
+//     count: 56
+//   },
+//   price: 799
+// },
+// {
+//   image: "images/products/black-2-slot-toaster.jpg",
+//   name : "2 Slot Toaster - Black",
+//   rating : {
+//     stars: 5,
+//     count: 2197
+//   },
+//   price: 1899
+// }
+// ];
 
 // we will use a forEach loop to loop thru each objects easily, and to use a function as a parameter
 
