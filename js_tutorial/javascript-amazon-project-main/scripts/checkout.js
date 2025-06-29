006aad783946aaaceeba404d41a04bf4c66f9ed6
Nicholas Gonzalez <nichoja20@gmail.com>
2025-06-29 09:04:37 -0400

    more date changes

--- js_tutorial/javascript-amazon-project-main/scripts/checkout.js
+++ js_tutorial/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import formatCurrency from '../scripts/utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -33,11 +33,17 @@ cart.forEach( (cartItem) => {
       }
     });
 
+
+
     const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays, 
-      "days"
-      );
+    const deliveryDate = deliveryOption
+    ? today.add(deliveryOption.deliveryDays, "days")
+    : today;
+
+    // const deliveryDate = today.add(
+    //   deliveryOption.deliveryDays, 
+    //   "days"
+    //   );
     const dateString = deliveryDate.format('dddd, MMMM D');
 
     cartSummaryHTML += `<div class="cart-item-container 
@@ -99,7 +105,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
 
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
-     html += `<div class="delivery-option">
+     html += `<div class="delivery-option js-delivery-option" 
+     data-product-id="${matchingProduct.id}" 
+     data-delivery-option-id="${deliveryOption.id}">
         <input type="radio" 
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -130,4 +138,12 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
       container.remove();
   })
 }
-)
+);
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+  element.addEventListener('click', ()=>{
+      const {productId, deliveryOptionId} = element.dataset;
+
+      updateDeliveryOption(productId, deliveryOptionId);
+  });
+});
