433c19e0620d6b71ac47d0ec0beaf6e0bfeac155
Nicholas Gonzalez <nichoja20@gmail.com>
2025-07-04 23:12:16 -0400

    object oriented programming

--- js_tutorial/javascript-amazon-project-main/scripts/checkout.js
+++ js_tutorial/javascript-amazon-project-main/scripts/checkout.js
@@ -1,5 +1,5 @@
 import {renderOrderSummary} from '../scripts/checkout/orderSummary.js';
 import {renderPaymentSummary} from '../scripts/checkout/paymentSummary.js';
-
+import '../data/cart-oop.js';
 renderOrderSummary();
 renderPaymentSummary();
