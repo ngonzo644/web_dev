4214ec7d68ef4a5b20eef1403209867f872bf8ff
Nicholas Gonzalez <nichoja20@gmail.com>
2025-07-08 20:53:48 -0400

    add backend practice file

--- js_tutorial/javascript-amazon-project-main/scripts/checkout.js
+++ js_tutorial/javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from '../scripts/checkout/orderSummary.js';
 import {renderPaymentSummary} from '../scripts/checkout/paymentSummary.js';
 //import '../data/cart-class.js';
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
