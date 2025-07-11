bd9c59a681022b9c7c7e53cc413f87138961bbbd
Nicholas Gonzalez <nichoja20@gmail.com>
2025-07-10 23:03:23 -0400

    used fetch instead of callback

--- js_tutorial/javascript-amazon-project-main/scripts/checkout.js
+++ js_tutorial/javascript-amazon-project-main/scripts/checkout.js
@@ -1,17 +1,15 @@
 import {renderOrderSummary} from '../scripts/checkout/orderSummary.js';
 import {renderPaymentSummary} from '../scripts/checkout/paymentSummary.js';
-import {loadProducts} from '../data/products.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
 //import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
-new Promise((resolve)=>{
-  loadProducts(()=>{
-    resolve();
-  });
-}).then(()=>{
-  renderOrderSummary();
-  renderPaymentSummary();
-});
+new Promise(()=>{
+  loadProductsFetch().then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+})
 
 
 // loadProducts(()=>{
